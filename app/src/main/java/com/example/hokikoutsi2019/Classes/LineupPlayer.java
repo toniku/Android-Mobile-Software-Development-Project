084e58006f1f1fd8caa8edebb6be4ae4182cdbc4
toniku <ossi.oppilas@oamk.fi>
2019-04-02 14:02:50 +0300

    Create LineupPlayer.java

---
+++ app/src/main/java/com/example/hokikoutsi2019/Classes/LineupPlayer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 Eetu, Janne, Jouni, Toni. All rights reserved. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.example.hokikoutsi2019.Classes;
+
+public class LineupPlayer {
+
+    private String firstName;
+    private String lastName;
+    private int jerseyNumber;
+    private int birthDate;
+
+    public LineupPlayer(String firstName, String lastName, int jerseyNumber, int birthDate) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setJerseyNumber(jerseyNumber);
+        this.setBirthDate(birthDate);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(int jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+}
+
+
