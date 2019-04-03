8afd9c142eeb3d2bd5e870687aeef1291a156a9a
jorse131 <jorse131@hotmail.com>
2019-04-03 11:09:17 +0300

    Update PlayerCard.java

--- app/src/main/java/com/example/hokikoutsi2019/PlayerCard.java
+++ app/src/main/java/com/example/hokikoutsi2019/PlayerCard.java
@@ -6,22 +6,27 @@ package com.example.hokikoutsi2019;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 public class PlayerCard extends AppCompatActivity {
 
-    private TextView playerPhoneNumberTextView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final TextView phoneNumberTextView = findViewById(R.id.playerPhoneNumberTextView);
 
-
-    public void onClick (View v) {
-        int viewID = v.getId();
-
-        if (viewID == R.id.playerPhoneNumberTextView) {
-            Uri number = Uri.parse("tel:555123");
-            Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
-            startActivity(callIntent);
-        }
+        phoneNumberTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneNumberTextView.getText().toString();
+                Uri number = Uri.parse(phoneNumber);
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
     }
 }
